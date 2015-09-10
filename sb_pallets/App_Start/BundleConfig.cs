c7f6824a92c302dca46adeae52352386a5db9e13
ashwinpal <alagappan.pal@gmail.com>
2015-09-10 19:55:12 -0400

    bug fix

--- sb_pallets/App_Start/BundleConfig.cs
+++ sb_pallets/App_Start/BundleConfig.cs
@@ -10,10 +10,10 @@ namespace sb_pallets
         {
             bundles.Add(new ScriptBundle("~/bundles/jquery").Include(
                         "~/Scripts/jquery-{version}.js"));
-
+/*
             bundles.Add(new ScriptBundle("~/bundles/jqueryui").Include(
                         "~/Scripts/jquery-ui-{version}.js"));
-
+            */
             bundles.Add(new ScriptBundle("~/bundles/jqueryval").Include(
                         "~/Scripts/jquery.unobtrusive*",
                         "~/Scripts/jquery.validate*"));
